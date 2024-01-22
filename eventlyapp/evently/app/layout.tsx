27daefc224e100d65269af02de4fdf76dad61b1c
sethu <callmesethu@gmail.com>
2024-01-22 09:49:40 +0530

    migrated clerk for authentication

--- eventlyapp/evently/app/layout.tsx
+++ eventlyapp/evently/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
 
 const poppins = Poppins({
@@ -22,8 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ eventlyapp/evently/components/shared/Header.tsx
@@ -0,0 +1,36 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently Logo"
+          />
+        </Link>
+        <div className="flex w-32 justify-end gap-3">
+          // after signedin this is visible
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          // after signedout this is visible
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
