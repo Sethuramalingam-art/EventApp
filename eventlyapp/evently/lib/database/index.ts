7ceefbcb31d093becd9b3c6348bbe17180eec156
sethu <callmesethu@gmail.com>
2024-01-25 08:18:42 +0530

    DB Connection initiated

---
+++ eventlyapp/evently/lib/database/index.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+//caching the DB connection
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDatabase = async () => {
+  //caching the DB connection
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+  //caching the DB connection.
+  //Workign with serverless architecture, server actions should create each and every time unwanted db connections
+  //so we can cach it to avoid multiple connections which use existing conection
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "Evently",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
---
+++ eventlyapp/evently/lib/database/models/event.model.ts
@@ -0,0 +1,20 @@
+import { Schema, model, models } from "mongoose";
+
+const EventSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  location: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  imageUrl: { type: String, required: true },
+  startDateTime: { type: Date, default: Date.now },
+  endDateTime: { type: Date, default: Date.now },
+  price: { type: String },
+  isFree: { type: Boolean, default: false },
+  url: { type: String },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+  organizer: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+const Event = models.Event || model("Event", EventSchema);
+
+export default Event;
