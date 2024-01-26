bbd033a71df1e554c1157f4648a0fc8cbc10021c
sethu <callmesethu@gmail.com>
2024-01-26 09:04:48 +0530

    server setup with vercel serverless functions

---
+++ eventlyapp/evently/lib/actions/users.actions.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import { CreateUserParams } from "@/types";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database";
+import User from "../database/models/user.model";
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    await connectToDatabase(); // cached DB connection
+    const newUser = await User.create(user);
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    handleError(error);
+  }
+};
--- eventlyapp/evently/lib/database/models/event.model.ts
+++ eventlyapp/evently/lib/database/models/event.model.ts
@@ -1,4 +1,20 @@
-import { Schema, model, models } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IEvent extends Document {
+  _id: string;
+  title: string;
+  description?: string;
+  location?: string;
+  createdAt: Date;
+  imageUrl: string;
+  startDateTime: Date;
+  endDateTime: Date;
+  price?: string;
+  isFree: boolean;
+  url?: string;
+  category: { _id: string; name: string };
+  organizer: { _id: string; firstName: string; lastName: string };
+}
 
 const EventSchema = new Schema({
   title: { type: String, required: true },
