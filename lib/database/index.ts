83bffc9f4f0659db884c4cb590003cef7290ba5b
sethu <callmesethu@gmail.com>
2024-01-28 12:01:55 +0530

    database name change commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -16,7 +16,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URI, {
-      dbName: "Evently",
+      dbName: "evently",
       bufferCommands: false,
     });
 
