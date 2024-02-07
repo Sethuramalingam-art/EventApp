ecfea8324582e1fb51c871557f593c50e661facd
sethu <callmesethu@gmail.com>
2024-02-07 10:49:06 +0530

    create event page completed

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,6 +5,22 @@ import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
 import { handleError } from "../utils";
+import Category from "../database/models/category.model";
+
+//to get the user details and category details using event findbyid
+const populateEvent = async (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({
+      path: "category",
+      model: Category,
+      select: "_id name",
+    });
+};
 
 export const createEvent = async ({
   event,
@@ -24,6 +40,20 @@ export const createEvent = async ({
       organizer: userId,
     });
     return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectToDatabase();
+    const event = await populateEvent(Event.findById(eventId));
+    if (!event) {
+      throw new Error("Event not found");
+    }
+
+    return JSON.parse(JSON.stringify(event));
   } catch (error) {
     handleError(error);
   }
