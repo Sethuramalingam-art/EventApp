9a85dced9d7d271b76cb83081719125f0c0ea1bd
sethu <callmesethu@gmail.com>
2024-02-14 08:34:20 +0530

    orders api integration

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -2,11 +2,17 @@
 
 import Stripe from "stripe";
 
-import { CheckoutOrderParams, CreateOrderParams } from "@/types";
+import {
+  CheckoutOrderParams,
+  CreateOrderParams,
+  GetOrdersByUserParams,
+} from "@/types";
 import { redirect } from "next/navigation";
 import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import Order from "../database/models/order.model";
+import User from "../database/models/user.model";
+import Event from "../database/models/event.model";
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
@@ -49,6 +55,45 @@ export const createOrder = async (order: CreateOrderParams) => {
       buyer: order.buyerId,
     });
     return JSON.parse(JSON.stringify(newOrder));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getOrdersByUser = async ({
+  userId,
+  limit = 3,
+  page,
+}: GetOrdersByUserParams) => {
+  try {
+    await connectToDatabase();
+
+    const skipAmount = (Number(page) - 1) * limit;
+    const conditions = { buyer: userId };
+
+    const orders = await Order.distinct("event._id")
+      .find(conditions)
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(limit)
+      .populate({
+        path: "event",
+        model: Event,
+        populate: {
+          path: "organizer",
+          model: User,
+          select: "_id firstName lastName",
+        },
+      });
+
+    const ordersCount = await Order.distinct("event._id").countDocuments(
+      conditions
+    );
+
+    return {
+      data: JSON.parse(JSON.stringify(orders)),
+      totalPages: Math.ceil(ordersCount / limit),
+    };
   } catch (error) {
     handleError(error);
   }
