e73c44d3483a7eb7a7b543b457766e5011f210b8
sethu <callmesethu@gmail.com>
2024-02-16 23:55:34 +0530

    events search component implementaion

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,25 +1,39 @@
 "use client";
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { Input } from "../ui/input";
-import { formUrlQuery } from "@/lib/utils";
-import { useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+import { clear } from "console";
 
-const Search = () => {
+const Search = ({
+  placeholder = "Search title...",
+}: {
+  placeholder?: string;
+}) => {
   const [query, setQuery] = useState("");
+  const router = useRouter();
   const searchParams = useSearchParams();
 
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
       if (query) {
-        const newUrl = formUrlQuery({
+        newUrl = formUrlQuery({
           params: searchParams.toString(),
           key: "query",
           value: query,
         });
+      } else {
+        newUrl = removeKeysFromQuery({
+          params: searchParams.toString(),
+          keysToRemove: ["query"],
+        });
       }
+      router.push(newUrl, { scroll: false });
     }, 300);
-  }, [query]);
+    return () => clearTimeout(delayDebounceFn);
+  }, [query, searchParams, router]);
   return (
     <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
       <Image
@@ -30,7 +44,7 @@ const Search = () => {
       />
       <Input
         type="text"
-        placeholder="Search"
+        placeholder={placeholder}
         onChange={(e) => setQuery(e.target.value)}
         className="p-regular-16 border-0 bg-grey-50 outline-offset-0 placeholder:text-grey-500 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
       />
