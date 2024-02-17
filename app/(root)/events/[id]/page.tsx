d4c6f3802e9bab36bfb9668c2dd0a8ff000677ef
sethu <callmesethu@gmail.com>
2024-02-17 14:06:57 +0530

    pagination fix done

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -101,9 +101,9 @@ const EventDetails = async ({
           emptyTitle="No Events Found"
           emptyStateSubText="Come back later"
           collectionType="All_Events"
-          limit={6}
-          page={1}
-          totalPages={2}
+          limit={3}
+          page={searchParams.page as string}
+          totalPages={relatedEvents?.totalPages}
         />
       </section>
     </>
